8c329121415127eb9348d20400bb405e90ba6265
Korhan Turgut <ateshan@yahoo.com>
2026-08-14 12:44:22 +0300

    fix(ci): align integration head and iOS device discovery timeout

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -13,7 +13,7 @@ REQUIRED_ENV_ROLES = {
     "TEST_ROUNDTRIP_DATABASE_URL": "db_owner",
 }
 
-EXPECTED_HEAD_REVISION = "031_analysis_job_claim_authority"
+EXPECTED_HEAD_REVISION = "033_worker_queue_visibility"
 
 
 async def run_real_preflight_checks():
